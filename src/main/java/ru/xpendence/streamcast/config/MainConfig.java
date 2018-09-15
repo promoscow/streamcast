477dbf823250917df78c4cbf280a3d68fb63a02e
chernyshov <chernyshov@bakapp.ru>
2018-09-15 21:36:23 +0300

    + MainConfig

---
+++ src/main/java/ru/xpendence/streamcast/config/MainConfig.java
@@ -0,0 +1,29 @@
+package ru.xpendence.streamcast.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
+
+/**
+ * Author: Vyacheslav Chernyshov
+ * Date: 15.09.18
+ * Time: 21:33
+ * e-mail: 2262288@gmail.com
+ */
+@Configuration
+public class MainConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(PRIVATE);
+        return mapper;
+    }
+}
