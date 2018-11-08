fb06c4753341378a04fa8c806c4c258c641afd61
Vyacheslav Chernyshov <vjatcheslav.chernyshev@stoloto.ru>
2018-11-08 14:58:17 +0300

    * in process

--- src/main/java/ru/xpendence/streamcast/aspect/ApiLogProcessor.java
+++ src/main/java/ru/xpendence/streamcast/aspect/ApiLogProcessor.java
@@ -2,7 +2,15 @@ package ru.xpendence.streamcast.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import ru.xpendence.streamcast.attributes.TransferType;
+import ru.xpendence.streamcast.domain.ApiLog;
+import ru.xpendence.streamcast.service.ApiLogService;
+import ru.xpendence.streamcast.util.JsonUtils;
+
+import java.time.LocalDateTime;
 
 /**
  * Author: Vyacheslav Chernyshov
@@ -14,8 +22,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApiLogProcessor {
 
+    private final ApiLogService service;
+
+    @Autowired
+    public ApiLogProcessor(ApiLogService service) {
+        this.service = service;
+    }
+
     @Before(value = "@annotation(request) && args(dto)")
     public void before(PostApiRequest request, Object dto) {
-        System.out.println();
+        service.save(ApiLog.of(
+                LocalDateTime.now(),
+                TransferType.REQUEST.name(),
+                HttpMethod.POST.name(),
+                getPath(),
+                JsonUtils.toJson(dto)
+        ));
+    }
+
+    //выяснить через рефлексию
+    private String getPath() {
+        return null;
     }
 }
---
+++ src/main/java/ru/xpendence/streamcast/attributes/TransferType.java
@@ -0,0 +1,12 @@
+package ru.xpendence.streamcast.attributes;
+
+/**
+ * Author: Vyacheslav Chernyshov
+ * Date: 08.11.2018
+ * Time: 14:54
+ * e-mail: vyacheslav.chernyshov@stoloto.ru
+ */
+public enum TransferType {
+    REQUEST,
+    RESPONSE;
+}
