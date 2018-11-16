6b45516cfbd673ddf77981e2da7bcdaf7747a10b
Vyacheslav Chernyshov <vjatcheslav.chernyshev@stoloto.ru>
2018-11-16 10:48:30 +0300

    * separate profile settings * fix @EnableJpaRepositories

--- src/main/java/ru/xpendence/streamcast/config/MainConfig.java
+++ src/main/java/ru/xpendence/streamcast/config/MainConfig.java
@@ -5,7 +5,6 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,7 +18,6 @@ import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
  */
 @Configuration
 @EnableAspectJAutoProxy
-@EnableJpaRepositories
 public class MainConfig {
 
     @Bean
